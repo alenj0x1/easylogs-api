6f38a1dd1275db9375194ae3eadbe4d977c80fbc
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2024-11-13 10:51:18 -0500

    fix: added missing Log classes by .gitignore

---
+++ easylogsAPI.Models/Requests/Log/CreateLogRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace easylogsAPI.Models.Requests.Log;
+
+public class CreateLogRequest
+{
+    [Required]
+    public string Message { get; set; } = null!;
+    [Required]
+    public string Trace { get; set; } = null!;
+    public string? Exception { get; set; }
+    public string? StackTrace { get; set; }
+    [Required] 
+    public int Type { get; set; } = 1;
+    public string DataJson { get; set; } = null!;
+}
---
+++ easylogsAPI.Models/Requests/Log/UpdateLogRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace easylogsAPI.Models.Requests.Log;
+
+public class UpdateLogRequest
+{
+    public string? Message { get; set; }
+    public string? Trace { get; set; }
+    public string? Exception { get; set; }
+    public string? StackTrace { get; set; }
+    public int? Type { get; set; }
+    public string? DataJson { get; set; }
+}
